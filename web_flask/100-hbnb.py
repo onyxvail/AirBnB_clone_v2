a5b926eb0c0e5f737502e9678525551f63814208
onyxvail <ons.hfi@gmail.com>
2023-10-22 15:16:28 +0100

    ADVANCED

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,15 +5,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def show_filters():
     """Shows filters"""
     from models.state import State
     from models.amenity import Amenity
+    from models.place import Place
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template("10-hbnb_filters.html",
-                            states=states,amenities=amenities)
+    places = storage.all(Place).values()
+    return render_template("100-hbnb.html", states=states,
+                           amenities=amenities, places=places)
 
 
 if __name__ == '__main__':
